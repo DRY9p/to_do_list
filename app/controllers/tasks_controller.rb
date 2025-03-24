2faa2f49aa626d5cb4b45bd0356baf3840906b94
Alex <alburton32@gmail.com>
2025-03-24 15:22:33 +0500

    stimulus flavored concept

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Задача успешно обновлена" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "Задача была успешно удалена из списка"
+  end
+
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(complited: params[:complited])
+
+    render json: { message: "Успешно выполнено" }
+  end
+
   private
 
   def task_params
